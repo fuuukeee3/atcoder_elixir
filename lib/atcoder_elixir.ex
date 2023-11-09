ca728a624567d7eaca6dda283504925f9eff2a03
fu <semarusika@gmail.com>
2023-11-10 06:18:39 +0900

    init

---
+++ lib/atcoder_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AtcoderElixir do
+  @moduledoc """
+  Documentation for `AtcoderElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AtcoderElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/atcoder_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AtcoderElixirTest do
+  use ExUnit.Case
+  doctest AtcoderElixir
+
+  test "greets the world" do
+    assert AtcoderElixir.hello() == :world
+  end
+end
