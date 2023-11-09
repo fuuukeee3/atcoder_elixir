2d2af94beb37a35c2551b10fd1d8125d3b70322e
fu <semarusika@gmail.com>
2023-11-10 08:44:11 +0900

    abc208 format

--- lib/abc208/a.exs
+++ lib/abc208/a.exs
@@ -13,7 +13,8 @@ defmodule Main do
 
   defp get_s, do: IO.read(:line) |> String.trim()
 
-  defp get_n_list, do:  IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
+  defp get_n_list,
+    do: IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
 end
 
 Main.main()
--- lib/abc208/b.exs
+++ lib/abc208/b.exs
@@ -11,15 +11,16 @@ defmodule Main do
   end
 
   defp factorial(0), do: 1
-  defp factorial(n), do: n * factorial(n-1)
+  defp factorial(n), do: n * factorial(n - 1)
 
   defp solve(list, p) do
-    result = Enum.reverse(list)
-    |> Enum.reduce(%{yen: p, count: 0}, fn(f_yen, acc) ->
-      div = div(acc.yen, f_yen)
-      mod = rem(acc.yen, f_yen)
-      new_acc = %{acc | yen: mod, count: acc.count + div}
-    end)
+    result =
+      Enum.reverse(list)
+      |> Enum.reduce(%{yen: p, count: 0}, fn f_yen, acc ->
+        div = div(acc.yen, f_yen)
+        mod = rem(acc.yen, f_yen)
+        new_acc = %{acc | yen: mod, count: acc.count + div}
+      end)
 
     IO.puts(result.count)
   end
@@ -28,7 +29,8 @@ defmodule Main do
 
   defp get_s, do: IO.read(:line) |> String.trim()
 
-  defp get_n_list, do:  IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
+  defp get_n_list,
+    do: IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
 end
 
 Main.main()
